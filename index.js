9d424aeefd1ce0ec2fb074af9f89ab60bc1631aa
sushil-verma <sushilvemra1985@gmail.com>
2021-04-10 22:43:42 +0530

    PanresponderFile

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './NewApp';
+import App from './PanResponderFile';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
